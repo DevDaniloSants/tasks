017762819ad37a2eb44d6128299e22893cda9358
Danilo Santos <dscofield@outlook.com.br>
2023-12-24 17:26:53 -0300

    Adiciona o menu do projeto, com a sua responsividade

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+// styles
+import '../styles/pages/rootLayout.sass';
+// hooks
+import { useMenu } from '../hooks/useMenu';
+// components
+import Menu from '../components/Menu';
+import MenuHamburguer from '../components/MenuHamburguer';
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  const { open, toggleMenu } = useMenu();
+
+  return (
+    <>
+      <header>
+        <Link to={'/'}>
+          My<span>Task</span>
+        </Link>
+        <div>
+          <Menu open={open} closeMenu={toggleMenu} />
+          <MenuHamburguer openMenu={toggleMenu} open={open} />
+        </div>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>&copy; Todos os direitos reservados - 2023 </footer>
+    </>
+  );
 };
 
 export default RootLayout;
