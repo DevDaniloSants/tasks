3675a457fa921aa3191708733fec45a335dd6b78
Danilo Santos <dscofield@outlook.com.br>
2023-12-24 17:29:18 -0300

    Adiciona style a página de home do projeto

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,17 @@
+import '../styles/pages/home.sass';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <h1>
+        My<span>Tasks</span>
+      </h1>
+      <Link to={'/new'}>
+        <button className="btn">Criar Tarefa</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
